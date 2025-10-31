a02fb765054da67f63391a249070e017f7fb93a9
Hammad Ahmed <46845004+HammadAhmedSherazi@users.noreply.github.com>
2025-10-31 22:01:52 +0500

    all store view screen set

--- lib/views/home/all_store_view.dart
+++ lib/views/home/all_store_view.dart
@@ -1,38 +1,75 @@
 import '../../export_all.dart';
 
-class AllStoreView extends StatelessWidget {
+class AllStoreView extends ConsumerStatefulWidget {
   final String title;
   const AllStoreView({super.key, required this.title});
 
   @override
-  Widget build(BuildContext context) {
-     final List<StoreDataModel> stores = List.generate(
-      16, // 4 items x 4 rows = 16
-      (index) => StoreDataModel(
-        title: "Abc Store",
-        address: "abc street",
-        rating: 4.5,
-        icon: Assets.store,
-      ),
-    );
+  ConsumerState<AllStoreView> createState() => _AllStoreViewState();
+}
 
-    return CustomScreenTemplate(title: title, child:GridView.builder(
-          // padding: const EdgeInsets.all(12),
+class _AllStoreViewState extends ConsumerState<AllStoreView> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      ref.read(homeProvider.notifier).getStores(limit: 20, skip: 0);
+    });
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        final homeState = ref.read(homeProvider);
+        if (homeState.getStoresApiResponse.status != Status.loadingMore) {
+          ref.read(homeProvider.notifier).getStores(
+            limit: 20,
+            skip: homeState.storesSkip,
+          );
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final homeState = ref.watch(homeProvider);
+    final stores = homeState.stores ?? [];
+
+    return CustomScreenTemplate(
+      title: widget.title,
+      child: AsyncStateHandler(
+        status: homeState.getStoresApiResponse.status,
+        dataList: stores,
+        itemBuilder: null,
+        scrollController: _scrollController,
+        padding: EdgeInsets.all(AppTheme.horizontalPadding),
+        customSuccessWidget: GridView.builder(
           shrinkWrap: true,
-          // physics: const NeverScrollableScrollPhysics(),
+          physics: const NeverScrollableScrollPhysics(),
           padding: EdgeInsets.all(AppTheme.horizontalPadding),
-          itemCount: stores.length,
-          gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, // 4 items per row
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
             mainAxisSpacing: 10.r,
             crossAxisSpacing: 10.r,
-
-            childAspectRatio: 0.87, // Adjust as per your card design
+            childAspectRatio: 0.87,
           ),
+          itemCount: stores.length,
           itemBuilder: (context, index) {
             final store = stores[index];
             return StoreCardWidget(data: store);
           },
-        ) );
+        ),
+        onRetry: () {
+          ref.read(homeProvider.notifier).getStores(limit: 20, skip: 0);
+        },
+      ),
+    );
   }
 }
